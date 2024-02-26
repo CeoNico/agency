20aa41d52083d3e1bfc367c6c6460a27ce333048
CeoNico <2004ssnicoo@gmail.com>
2024-02-26 17:36:23 -0500

    Fonts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,35 @@
 import Image from "next/image";
 import React from "react";
 import { teamPng } from "../../public";
+import { Crimson_Text } from "next/font/google";
+
+const crimson = Crimson_Text({
+  weight: "400",
+  subsets: ["latin"],
+  fallback: "sans-serif",
+});
 
 const About = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-5 justify-evenly items-center">
+    <div
+      className={`${crimson.className} flex flex-col lg:flex-row gap-5 justify-evenly items-center p-4`}
+    >
       <div className="w-full flex flex-col p-2 justify-center items-center ">
-        <h1 className="text-white text-center text-2xl font-bold p-4">
-          Sobre Nosotros
-        </h1>
-        <div className="lg:w-[400px] lg:text-pretty">
-        <p className="text-white text-xl font-light">
-          Somos una agencia que desarolla sitios webs para que emprendedores
-          Argentinos aumenten su presencia en linea.
-          Muchos Argentinos no tienen un sitio web para demostrar sus servicio o
-          producto y nosotros estamos dedicado a cambiar eso. Los sitio webs
-          facilita la interacion con los clientes y habilita un manera mas
-          efficiente para conectarse con clientes!
-        </p>
+        <div className="max-w-[400px] lg:text-pretty">
+          <h1 className="text-white font-extrabold text-4xl">
+            Sobre Nosotros
+          </h1>
+          <p className="text-white text-xl">
+            Somos una agencia que desarolla sitios webs para que emprendedores
+            Argentinos aumenten su presencia en linea. Muchos Argentinos no
+            tienen un sitio web para demostrar sus servicio o producto y
+            nosotros estamos dedicado a cambiar eso. Los sitio webs facilita la
+            interacion con los clientes y habilita un manera mas efficiente para
+            conectarse con clientes!
+          </p>
         </div>
       </div>
-      <div className="max-w-lg">
+      <div className="max-w-lg p-6">
         <Image className="rounded-3xl shadow-lg shadow-black" src={teamPng} />
       </div>
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,17 @@
+import { DM_Serif_Display } from "next/font/google";
 import Button from "./Button";
+
+const serif = DM_Serif_Display({fallback:["sans-serif"], weight:"400", subsets: ["latin"] });
 
 const Hero = () => {
   return (
-    <div className="h-[500px] lg:h-full flex justify-evenly">
+    <div className={`${serif.className}  h-[500px] lg:h-full flex justify-evenly`}>
       <div className="bg-hero bg-cover bg-center w-full items-center flex justify-center">
-        <div className="w-[80%] h-[70%] lg:w-[50%] bg-white/20 rounded-xl items-center justify-center -mt-20">
-          <h1 className="p-2 text-white text-2xl text-center font-bold">
+        <div className="w-[80%] h-fit lg:w-[50%] bg-white/10 rounded-xl items-center justify-center -mt-20">
+          <h1 className="p-2 text-orange-600 text-4xl text-center font-bold">
             Listo Para Llevar Tu Negocio Al Proximo Nivel?
           </h1>
-          <p className="text-white font-light text-center text-lg p-2">
+          <p className="text-white font-light text-center text-xl p-2">
             Empeza la transicion hacia un mundo nuevo.... el mundo digital.
             Construi tu propio sitio web hoy! Sin ningun terciario{" "}
           </p>
